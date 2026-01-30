e1238f450eb381bb7fcb1ea1d87dade6ff003cb4
Jose Eduardo Madrigal Osorio <75703758+Eduardo-Osoi@users.noreply.github.com>
2026-01-30 09:25:56 -0600

    Add files via upload

---
+++ Sistemalalo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema;
+import sistema.empleadosGUI.frmAcceso;
+/**
+ *
+ * @author Eduardo
+ */
+public class Sistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmAcceso P = new frmAcceso();
+        P.setVisible(true);
+    }
+    
+}
